5e671fb300cc572e0aa3f1f0344cead9edfef738
francisco784 <pfran2005@gmail.com>
2023-07-06 13:11:25 -0300

    cambios en java y CSS

--- carrito.js
+++ carrito.js
@@ -2,54 +2,80 @@ let precio = 0;
 let arrayProductos = [];
 
 let sumarCarrito = (nombre, checkeado, valor) => {
-  
+console.log(nombre, checkeado, valor)
     if (checkeado) {
-        precio+=Number(valor);
+        precio += Number(valor);
         arrayProductos.push(nombre)
-    } else{
-        precio-=valor
+    } else {
+        precio -= valor
         arrayProductos.pop(nombre)
 
     }
     console.log(precio)
     console.log(JSON.stringify(arrayProductos))
-
+    sumarPrecios()
 }
 
+sumarPrecios = () => {
+   let articuloscarrito1 = document.getElementById("articuloscarrito");
+    articuloscarrito1.innerHTML = " " +  arrayProductos.join(",");
 
+    let totalcarrito1 = document.getElementById("totalcarrito");
+    totalcarrito1.textContent = "total; " + suma;
 
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class carrito{
-    sumarCarrito8(e){
-     e.preventDefault();
-     if(e.target.classlist.contains ("agregar carrito")){
-        const producto = e.target.parentElement.parentElement;
-        this.leerdatosproducto(producto)
-     }
+    const textocarrito1 = document.getElementById("textocarrito");
+    if (articuloscarrito1.length === 0){
+        textocarrito1.textContent= "el carrito se encuentra vacio";
+    } else{
+      textocarrito1.textContent = "";
 
     }
-
 }
+//desplegar div
+
+let botondesplegar1 = document.getElementById("botondesplegar");
+let contenidodesplegable1 = document.getElementById("contenidodesplegable");
+
+botondesplegar1.addEventListener("click", () => {
+   console.log("no se muestra nada")
+    if (contenidodesplegable1.style.display === "none") {
+        contenidodesplegable1.style.display = "block";
+
+    } else {
+
+        contenidodesplegable1.style.display = "none";
+    }
+
+});
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
